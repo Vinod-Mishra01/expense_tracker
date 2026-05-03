9fb671db94e49120ad503768abac62e5cc3da11c
Vinod Mishra <vinodmishra650103@gmail.com>
2026-05-04 01:48:04 +0530

    auth controller transporter cod updated

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -426,14 +426,21 @@ exports.forgotPassword = async (
         //         },
         //     })
 
-        const transporter =
+const transporter =
 nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false,
+    },
     connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 })
 
         await transporter.sendMail({
